2366552c9bda056ca38114b4d0baffed0b57cc10
Paco Alba <kiviev@gmail.com>
2018-06-18 21:52:28 +0200

    Create Api Routes Notes CRUD

---
+++ routes/api/v1/note.router.js
@@ -0,0 +1,48 @@
+const KoaRouter = require('koa-router');
+const logger = require('logger.js');
+
+const router = new KoaRouter({
+  prefix: process.env.API_VERSION + '/notes'
+});
+
+class NoteRouter{
+
+    static async getNotes(ctx) {
+        logger.info('[API-NoteRouter]@getNotes');
+        ctx.body = 'getNotes';
+    }
+
+    static async getNoteById(ctx) {
+       logger.info('[API-NoteRouter]@getNoteById');
+       ctx.body = 'getNoteById';
+    }
+    
+    static async createNote(ctx) {
+        logger.info('[API-NoteRouter]@createNote');
+        ctx.body = 'createNote';
+    }
+
+    static async updateNote(ctx) {
+        logger.info('[API-NoteRouter]@updateNote');
+        ctx.body = 'updateNote';
+    }
+
+    static async deleteNote(ctx) {
+      logger.info('[API-NoteRouter]@deleteNote');
+      ctx.body = 'deleteNote';
+    }
+
+    static async setFavNote(ctx) {
+        logger.info('[API-NoteRouter]@setFavNote');
+        ctx.body = 'setFavNote';
+    }
+
+}
+
+router.get('/', NoteRouter.getNotes);
+router.get('/:id', NoteRouter.getNoteById);
+router.post('/', NoteRouter.createNote);
+router.put('/:id', NoteRouter.updateNote);
+router.delete('/:id', NoteRouter.deleteNote);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,15 +3,21 @@ require('dotenv').config();
 const Koa = require('koa');
 const koaLogger = require('koa-logger');
 const logger = require('logger.js');
+const apiversion = process.env.API_VERSION;
 
+// Routers
+const notesApiRouter = require('routes/' + apiversion + '/note.router');
 
 const app = new Koa();
 const port = process.env.PORT;
 
+
+app.use(notesApiRouter.middleware());
+
 app.listen(port, (err) => {
     logger.info('[Server] Init');
   if (err) {
-    // console.log(err);
+    logger.info('[Server] Init Error:', err);
   }
   console.log('Listening in port: ' + port);
 });
